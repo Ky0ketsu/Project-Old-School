a5ac548f0ce8cbcf31ab6653c507049d51947752
Antoine <>
2025-10-30 15:46:02 +0100

    Intégration SFX Camera Open/Close

--- SYG_Project/Assets/-SCRIPTS———————————————————————/Scr_TabletMove.cs
+++ SYG_Project/Assets/-SCRIPTS———————————————————————/Scr_TabletMove.cs
@@ -8,6 +8,14 @@ public class Scr_TabletMove : MonoBehaviour
     public Vector3 initTransform;
     public Transform selfTransform; 
 
+    
+
+    public List<AudioClip> cameraActionOpenList;
+    public List<AudioClip> cameraActionCloseList;
+    [SerializeField] AudioSource audioCameraAction;
+    public GameObject prefab;
+
+
     private void Start()
     {
         initTransform = transform.position;
@@ -16,6 +24,14 @@ public class Scr_TabletMove : MonoBehaviour
     }
     public void Update()
     {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            int r = Random.Range(0, cameraActionOpenList.Count);
+            audioCameraAction.clip = cameraActionOpenList[r];
+            audioCameraAction.Play();
+        }
+
+
         if (Input.GetKey(KeyCode.E))
         {
             selfTransform.DOLocalMove(new Vector3(transform.position.x, 0, transform.position.z), 0.5f, false);
@@ -24,6 +40,10 @@ public class Scr_TabletMove : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.E))
         {
             selfTransform.DOLocalMove(new Vector3(0, transform.position.y - 1000, 0), 0.5f, false);
+
+            int r = Random.Range(0, cameraActionCloseList.Count);
+            audioCameraAction.clip = cameraActionCloseList[r];
+            audioCameraAction.Play();
         }
     }
 }
