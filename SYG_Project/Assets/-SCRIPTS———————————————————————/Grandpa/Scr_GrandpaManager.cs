00cfdb9169373eec32a0975780a0bdea395237de
Linkdu <chaillouxmarius@gmail.com>
2026-01-07 13:44:15 +0100

    update scene + win

--- SYG_Project/Assets/-SCRIPTS———————————————————————/Grandpa/Scr_GrandpaManager.cs
+++ SYG_Project/Assets/-SCRIPTS———————————————————————/Grandpa/Scr_GrandpaManager.cs
@@ -34,15 +34,22 @@ public class Scr_GrandpaManager : MonoBehaviour
             grandpas[i] = grandpaParent.GetChild(i).gameObject;
 
 
-            if (grandpas[i].GetComponent<Scr_GranpaOrigin>() != null && parentBedroom.GetChild(i) != null)
+            if (grandpas[i].GetComponent<Scr_GrandpaOrigin>() != null && parentBedroom.GetChild(i) != null)
             {
-                grandpas[i].GetComponent<Scr_GranpaOrigin>().player = player;
+                grandpas[i].GetComponent<Scr_GrandpaOrigin>().player = player;
                 Bedrooms[i] = parentBedroom.GetChild(i);
-                grandpas[i].GetComponent<Scr_GranpaOrigin>().bedroom = Bedrooms[i];
+                grandpas[i].GetComponent<Scr_GrandpaOrigin>().bedroom = Bedrooms[i];
             }
             else Debug.Log($"1 ou 2 elements manquant dans la paire chambre/vieux numero {i}");
         }
     }
 
-
+    public void CountGrandpa()
+    {
+        if (grandpas[0].GetComponent<Scr_Mimolle>().inBedroom == true && grandpas[1].GetComponent<Scr_CrazyCat>().inBedroom == true &&
+            grandpas[2].GetComponent<Scr_Rex>().inBedroom == true && grandpas[3].GetComponent<Scr_Speeder>().inBedroom == true)
+        {
+            Debug.Log("win");
+        }
+    }
 }
