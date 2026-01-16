81b9fa3f5b0836f003753860a75a1c2a30f30557
Antoine <>
2026-01-16 14:44:37 +0100

    Correction Son Ambiance light, le son ne se lance plus dans le menu

--- SYG_Project/Assets/Scr_Amb_Light.cs
+++ SYG_Project/Assets/Scr_Amb_Light.cs
@@ -10,17 +10,24 @@ public class Scr_Amb_Light : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        EVENTS.OnGameplay += PlaySound;
+        
+        
+
+    }
+
+    void PlaySound()
+    {
         int r = Random.Range(0, buzzList.Count);
         AudioClip clip = buzzList[r];
         buzz.clip = clip;
         buzz.Play();
-
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void OnDestroy()
     {
-            
-        
+        EVENTS.OnGameplay -= PlaySound;
     }
 }
