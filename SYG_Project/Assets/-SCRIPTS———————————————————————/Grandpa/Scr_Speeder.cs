e9e78565423467ea6bdb59e9338e27034bb4158b
Ky0h0 <daymondeschamps@gmail.com>
2026-01-19 23:22:24 +0100

    a ignorer

--- SYG_Project/Assets/-SCRIPTS———————————————————————/Grandpa/Scr_Speeder.cs
+++ SYG_Project/Assets/-SCRIPTS———————————————————————/Grandpa/Scr_Speeder.cs
@@ -49,12 +49,12 @@ public class Scr_Speeder : GrandpaParent
                 Debug.Log(dir[currentDirIndex]);
                 Debug.Log(currentPickPosition);
 
-                Debug.DrawLine(lastPickPosition, currentPickPosition, Color.yellow, 1f);
+               // Debug.DrawLine(lastPickPosition, currentPickPosition, Color.yellow, 1f);
 
                 if (NavMesh.SamplePosition(currentPickPosition, out hit, 1f, NavMesh.AllAreas))
                 {
                     currentIndex++;
-                    Debug.DrawLine(transform.position, hit.position,Color.green,1f);
+                    //Debug.DrawLine(transform.position, hit.position,Color.green,1f);
                 }
                 else
                 { 
@@ -67,6 +67,7 @@ public class Scr_Speeder : GrandpaParent
             {
                 break;
             }
+            Debug.DrawLine(transform.position, lastPickPosition, Color.white, 1f);
             targetPosition = lastPickPosition;
             mouvIsSet = true;
 
