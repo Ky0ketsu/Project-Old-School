205ddb48e43ec7239af327201e040ff626b8dd6a
Linkdu <chaillouxmarius@gmail.com>
2026-01-22 10:35:57 +0100

    La scène est pas bzar + tuto en fr et en

--- SYG_Project/Assets/-SCRIPTS———————————————————————/Player Controller Actions/Scr_Tuto.cs
+++ SYG_Project/Assets/-SCRIPTS———————————————————————/Player Controller Actions/Scr_Tuto.cs
@@ -25,7 +25,6 @@ public class Scr_Tuto : MonoBehaviour
     private Player player;
 
     public GameObject TutoText;
-    TextMeshProUGUI textmeshpro;
 
     [SerializeField] PlayerActionService playerActionService;
     [SerializeField] Scr_GameTimer Scr_GameTimer;
@@ -37,7 +36,7 @@ public class Scr_Tuto : MonoBehaviour
     private void Start()
     {
         player = ReInput.players.GetPlayer(0);
-        textmeshpro = TutoText.GetComponent<TextMeshProUGUI>();
+
         Scr_GameTimer = FindAnyObjectByType<Scr_GameTimer>();
         DoorToOpen.GetComponent<Scr_FireDoor>().CAN_BE_CLOSED = false;
 
@@ -68,8 +67,8 @@ public class Scr_Tuto : MonoBehaviour
                 gameObject.GetComponent<PlayerMove>().enabled = false;
                 gameObject.GetComponent<PlayerLook>().enabled = false;
             }
-           
-            textmeshpro.enabled = true; 
+
+            TutoText.GetComponent<TextMeshProUGUI>().enabled = true;
 
         }
 
@@ -94,7 +93,11 @@ public class Scr_Tuto : MonoBehaviour
 
         if (didHeSlap == true)
         {
-            textmeshpro.SetText("They go to their room and will come out of it after some time \n Hold Space or Left Click to look at your TABLET"); 
+
+            TutoText.GetComponent<TextMeshProUGUI>().enabled = false;
+            TutoText = GameObject.Find("TABLET");
+            TutoText.GetComponent<TextMeshProUGUI>().enabled = true;
+
             GameObject TabletGO = scriptTablet.gameObject;
             TabletGO.GetComponent<TabletAction>().enabled = true;
 
@@ -120,12 +123,20 @@ public class Scr_Tuto : MonoBehaviour
             didHeSlap = false;
             gameObject.GetComponent<PlayerMove>().enabled = true;
             gameObject.GetComponent<PlayerLook>().enabled = true;
-            textmeshpro.SetText("Seems like other grandpas need to be cured!! \n Bring them back to their room before the end of the timer on your TABLET");
+
+            TutoText.GetComponent<TextMeshProUGUI>().enabled = false;
+            TutoText = GameObject.Find("RULES");
+            TutoText.GetComponent<TextMeshProUGUI>().enabled = true;
+
             timerForTextTuto += Time.deltaTime;
             if (timerForTextTuto > 7)
             {
                 didHeCamera = false;
-                textmeshpro.SetText("SLAP the door to start \n And you better close behind you, insolent >:c");
+
+                TutoText.GetComponent<TextMeshProUGUI>().enabled = false;
+                TutoText = GameObject.Find("START");
+                TutoText.GetComponent<TextMeshProUGUI>().enabled = true;
+
                 DoorToOpen.GetComponent<Scr_FireDoor>().CAN_BE_CLOSED = true;
             }
 
